ec46866706e57a223c2bbeea0b493dceb440bc4b
Zak-F48 <zakfeingold@hotmail.co.uk>
2025-11-19 10:45:33 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,26 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	      int number = Integer.parseInt(args[0]);
+        double sum = 1.0;
+        int currentIndex = 3;
+        boolean plus = false;
+
+        for (int i = 1; i < number; i++) {
+            if (plus) {
+                sum += 1.0 / currentIndex;
+                plus = false;
+            } else {
+                sum -= 1.0 / currentIndex;
+                plus = true;
+            }
+            currentIndex += 2;
+        }
+
+        double approxPi = 4 * sum;
+
+        System.out.println("pi according to Java: " + Math.PI);
+        System.out.println("pi, approximated:     " + approxPi);
+    }
 	}
-}
+
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,15 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String givenTime = args[0]; 
+        int addedMinutes = Integer.parseInt(args[1]);
+        String[] timeParts = givenTime.split(":");
+        int hours = Integer.parseInt(timeParts[0]);
+        int minutes = Integer.parseInt(timeParts[1]);
+        int totalMinutes = (hours * 60 ) + minutes + addedMinutes;
+        int totalHours = (totalMinutes / 60 );
+        int newHours = totalHours % 24;
+        int newMinutes = totalMinutes - (totalHours * 60);
+        System.out.print(newHours + ":" + newMinutes);
+
     }
 }
